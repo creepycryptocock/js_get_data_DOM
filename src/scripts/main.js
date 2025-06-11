69ee725101b4dfffcf8eb05609c3d1de505e5e56
Yurii <j.zherebilov15@gmail.com>
2025-06-11 16:26:16 +0300

    fix selectors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const populations = [...document.querySelectorAll('population')];
+const populations = [...document.querySelectorAll('.population')];
 
 const total = populations.reduce((acc, val) => +acc + +val, 0);
 const average = total / populations.length;
 
-const totalPopulation = document.querySelector('total-population');
-const averagePopulation = document.querySelector('average-population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
 
 totalPopulation.textContent = total;
 averagePopulation.textContent = average;
