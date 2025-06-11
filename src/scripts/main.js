94090f925ea748a6b6ba7d68cbfb1badaf744d21
Yurii <j.zherebilov15@gmail.com>
2025-06-11 16:43:37 +0300

    fix selectors and populations parsing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const populations = [...document.querySelectorAll('.population')];
+const populations = [...document.querySelectorAll('.population')]
+.map(el => +el.textContent.replace(/,/g, ''));
 
-const total = populations.reduce((acc, val) => +acc + +val, 0);
+const total = populations.reduce((acc, val) => acc + val, 0);
 const average = total / populations.length;
 
 const totalPopulation = document.querySelector('.total-population');
 const averagePopulation = document.querySelector('.average-population');
 
-totalPopulation.textContent = total;
-averagePopulation.textContent = average;
+totalPopulation.textContent = total.toLocaleString();
+averagePopulation.textContent = average.toLocaleString();
